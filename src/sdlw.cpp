c8b06d49665da7120db1169e101f2a619690ea06
doremi <doremi@ubuntu>
2011-09-29 11:16:28 +0800

    改 button 位置

--- src/sdlw.cpp
+++ src/sdlw.cpp
@@ -64,15 +64,15 @@ int main(int argc, char *argv[])
 	Gui = new TGui(screen);
 
 	// add few elements
-	Gui->AddElement( new TButton(Gui, 10, 571, 60, 18, "btn1", "CLICK") );
+	Gui->AddElement( new TButton(Gui, 10, 80, 60, 18, "btn1", "CLICK") );
 	Gui->AddElement( new TButton(Gui, 75, 571, 60, 18, "btn2", "CLICK") );
 	Gui->AddElement( new TButton(Gui, 140, 571, 60, 18, "btn3", "CLICK") );
 	Gui->AddElement( new TButton(Gui, 205, 571, 60, 18, "btn4", "CLICK") );
 	Gui->AddElement( new TButton(Gui, 270, 571, 60, 18, "btn5", "CLICK") );
 
-//	Gui->AddElement( new TWindow(Gui, 10, 10, 200, 200, "wnd1", "Hello!") );
+	Gui->AddElement( new TWindow(Gui, 10, 10, 50, 50, "wnd1", "Hello!") );
 //	Gui->AddElement( new TWindow(Gui, 30, 30, 150, 150, "wnd2", "Hello w2!") );
-	Gui->AddElement( new TWindow(Gui, 0, 0, 480, 272, "wnd3", "Hello w3!") );
+//	Gui->AddElement( new TWindow(Gui, 0, 0, 480, 272, "wnd3", "Hello w3!") );
 
 	// and show the results
 	Gui->Redraw();
