662ab9de4596a017e72bd4cd189b8ff468ad6f5a
doremi <doremi@ubuntu>
2011-10-03 15:20:03 +0800

    可以畫出 freetype 的字
    * 位置和大小可能還需要調整
    * 背景被蓋掉了

--- src/TText.cpp
+++ src/TText.cpp
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <wchar.h>
 #include "TGui.h"
 #include "TGuiElement.h"
 #include "TText.h"
@@ -8,18 +9,29 @@
 #define CHAR_SPACING 3
 #define LINE_SPACING 3
 
+#define FONT_SIZE 16
+#define FONT_FILE "/usr/share/cups/fonts/Monospace"
+
 TText::TText(TGui *Parent, int x, int y, char *name, char *str)
-//    : TGuiElement(Parent, x, y, width, height, name)
-    : TGuiElement(Parent, x, y, (8+CHAR_SPACING)*strlen(str), (8+LINE_SPACING), name)
+//    : TGuiElement(Parent, x, y, (8+CHAR_SPACING)*strlen(str), (8+LINE_SPACING), name)
+    : TGuiElement(Parent, x, y, 100, 30, name)
 {
     this->str = strdup(str);
 
-    Col.Border.r = 0x00; Col.Border.g = 0x00; Col.Border.b = 0x00;
-    Col.CapBkg.r = 0xF0; Col.CapBkg.g = 0xA0; Col.CapBkg.b = 0x00;
-    Col.CapTxt.r = 0xff; Col.CapTxt.g = 0xff; Col.CapTxt.b = 0xff;
-    Col.CliBkg.r = 0xC0; Col.CliBkg.g = 0xC0; Col.CliBkg.b = 0xC0;
-    Col.Light3D.r = 0xff; Col.Light3D.g = 0xff; Col.Light3D.b = 0xff;
-    Col.Dim3D.r = 0x40; Col.Dim3D.g = 0x40; Col.Dim3D.b = 0x40;
+    FT_Init_FreeType( &library );
+    FT_New_Face( library, FONT_FILE, 0, &face );
+    FT_Set_Char_Size(face, FONT_SIZE*32, 0, 400, 0);
+    slot = face->glyph;
+
+    pen.x =  0;
+    pen.y = 0;
+
+    FT_Error error = FT_Select_Charmap( face, FT_ENCODING_UNICODE);
+    if ( error != 0 ) {
+        printf("select font error");
+        perror("select font");
+        exit(1);
+    }
 
     SDL_SetAlpha(surface, SDL_SRCALPHA, 255);
 
@@ -29,6 +41,8 @@ TText::TText(TGui *Parent, int x, int y, char *name, char *str)
 TText::~TText()
 {
     free(str);
+    FT_Done_Face(face);
+    FT_Done_FreeType(library);
 }
 
 uint32_t get_font(unsigned char ascii)
@@ -41,6 +55,66 @@ const struct font_desc *TText::fonts[] = {
     &font_vga_8x8
 };
 
+void TText::draw_bitmap( FT_Bitmap *bitmap, FT_Int x, FT_Int y)
+{
+    Uint32 color;
+    FT_Int  i, j, p, q;
+    FT_Int  x_max = x + bitmap->width;
+    FT_Int  y_max = y + bitmap->rows;
+
+    if (SDL_MUSTLOCK(surface)) {
+        if (SDL_LockSurface(surface) < 0) {
+            fprintf(stderr, "Can't lock screen: %s\n", SDL_GetError());
+            return;
+        }
+    }
+
+    for ( i = x, p = 0; i < x_max; i++, p++ ) {
+        for ( j = y, q = 0; j < y_max; j++, q++ ) {
+            if ( i >= width || j >= height )
+                continue;
+            if (bitmap->buffer[q * bitmap->width + p]) {
+                color = SDL_MapRGB(surface->format, bitmap->buffer[q * bitmap->width + p], 0, 0);
+                putpixel(surface, i, j, color);
+            }
+        }
+    }
+
+    if (SDL_MUSTLOCK(surface)) {
+        SDL_UnlockSurface(surface);
+    }
+}
+
+void TText::drawtext(wchar_t *text)
+{
+    int num_chars, n;
+    num_chars = wcslen( text );
+
+    for ( n = 0; n < num_chars; ++n ) {
+        FT_Set_Transform( face, NULL, &pen );
+        FT_Load_Char( face, text[n], FT_LOAD_RENDER );
+
+        draw_bitmap( &slot->bitmap,
+                 slot->bitmap_left,
+                 height - slot->bitmap_top );
+
+        pen.x += slot->advance.x;
+        pen.y += slot->advance.y;
+    }
+
+    // move to new line
+    pen.y -= FONT_SIZE *2 * 64;
+    pen.x  = 50 * 64;
+
+}
+
+void TText::Draw()
+{
+    wchar_t text[] = L"Fuck";
+    drawtext(text);
+}
+
+#if 0
 void TText::Draw()
 {
     Uint32 yellow = SDL_MapRGB(surface->format, 255, 0, 0);
@@ -73,3 +147,4 @@ void TText::Draw()
         SDL_UnlockSurface(surface);
     }
 }
+#endif
--- src/TText.h
+++ src/TText.h
@@ -1,5 +1,8 @@
-#ifndef TTextH
-#define TTextH
+#ifndef TTEXTH
+#define TTEXTH
+
+#include <ft2build.h>
+#include <freetype2/freetype/freetype.h>
 
 class TText : public TGuiElement
 {
@@ -11,5 +14,14 @@ private:
     char *str;
     TColors Col;
     static const struct font_desc *fonts[];
+
+// FreeType
+    FT_Library library;
+    FT_Face face;
+    FT_GlyphSlot slot;
+    FT_Vector pen;
+    void drawtext(wchar_t *text);
+    void draw_bitmap( FT_Bitmap *bitmap, FT_Int x, FT_Int y);
 };
+
 #endif
