14006710a5808f041880b637761fca4a14bf5e61
doremi <doremi@ubuntu>
2011-11-22 09:12:39 +0800

    修正 RGB 頁面的顯示順序
    * 以及 "離開" RGB page 時, 要先畫第一個紅色頁面
      (或其他圖形, 這東西會在從別頁 "跳進" RGB page 馬上顯示出來,
       因此 index 也要修正)

--- src/RGBPage.cpp
+++ src/RGBPage.cpp
@@ -9,20 +9,14 @@
 
 
 RGBPage::RGBPage(int page, SDL_Surface *background)
-    : Pages(page, background)
+    : Pages(page, background),
+      canvas(NULL)
 {
-//    rgb_button = new TButton(this, 0, 0, 480, 272, "rgb-btn", "");
-//    rgb_button->setClicked(switch_rgb);
-//    rgb_button->setVisible(false);
-
     Functor<TCanvas::CallbackType> draw_cmd(this, &RGBPage::onDraw);
     Functor<TCanvas::CallbackType> click_cmd(this, &RGBPage::onClick);
 
     canvas = new TCanvas(this, 0, 0, 480, 272, draw_cmd);
-//    canvas->setDrawed(draw_cmd);
     canvas->setClicked(click_cmd);
-
-//    change_color();
 
     addElement(canvas);
 }
@@ -34,10 +28,6 @@ int RGBPage::get_next_page() const
 
 void RGBPage::onClick(SDL_Surface *surface)
 {
-//    if (change_color() == true) {
-//        PageManager::getInstance().set_switch_button(rgb_button, get_next_page());
-//    }
-
     if (change_color(surface) == true) {
         PageManager::getInstance().switch_to(get_next_page());
     }
@@ -64,25 +54,10 @@ bool RGBPage::change_color(SDL_Surface *surface)
         {0x80, 0x80, 0x80, 0x00}
     };
 
-#if 0
-    if (i >= (sizeof(color)/sizeof(color[1]))) {
-        i = 1;
-        SDL_FillRect(surface, NULL, GetCol(surface, color[0]));
-        return true;
-    }
-#endif
-
-    switch (index) {
-        case 0:
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-            SDL_FillRect(surface, NULL, GetCol(surface, color[index]));
-            break;
-        default:
-            SDL_FillRect(surface, NULL, GetCol(surface, color[5]));
-            break;
+    if (index <= PAGE_BLACK) {
+        SDL_FillRect(surface, NULL, GetCol(surface, color[index]));
+    } else {
+        SDL_FillRect(surface, NULL, GetCol(surface, color[5]));
     }
 
     switch (index) {
@@ -148,6 +123,12 @@ bool RGBPage::change_color(SDL_Surface *surface)
 
         default:
             break;
+    }
+
+    if (index > PAGE_LAST) {
+        index = 1;
+        SDL_FillRect(surface, NULL, GetCol(surface, color[0]));
+        return true;
     }
 
     ++index;
--- src/RGBPage.h
+++ src/RGBPage.h
@@ -10,6 +10,7 @@ class RGBPage : public Pages {
 public:
     RGBPage(int page, SDL_Surface *background = NULL);
     virtual int get_next_page() const;
+
 private:
     TCanvas *canvas;
 
@@ -18,6 +19,8 @@ private:
 
     bool change_color(SDL_Surface *surface);
     void rgb_clicked(void *data);
+
+// no need to implement
     virtual void onTimerEvent() {}
     virtual TButton *get_switch_next_button() const { return NULL; }
 
