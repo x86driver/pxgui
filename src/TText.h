8aad6e4726511a3c878b0669ffa738d9546355d0
doremi <doremi@ubuntu>
2011-11-23 11:28:46 +0800

    使用 FontManager 管理所有字型
    一開始會產生三種大小, 16, 20, 24
    這樣的缺點是無法動態改變大小
    但可解決太多字型檔被同時 open 的問題
    也可解決 memory 使用量

--- src/TText.h
+++ src/TText.h
@@ -5,6 +5,29 @@
 #include <ft2build.h>
 #include <freetype2/freetype/freetype.h>
 
+#if defined(DOREMI_USE_USTL)
+#  include <umap.h>
+using namespace ustl;
+#else
+#  include <map>
+using namespace std;
+#endif
+
+class FontManager {
+public:
+    static FontManager &getInstance();
+    TTF_Font *getFont(int ptsize);
+
+private:
+    map<int, TTF_Font*> m;
+
+    FontManager();
+    ~FontManager();
+
+    FontManager(const FontManager &);
+    void operator=(const FontManager &);
+};
+
 class TText : public TGuiElement
 {
 public:
@@ -12,7 +35,6 @@ public:
     ~TText();
     void Draw();
     SDL_Surface *render();
-    void setfontsize(int size);
     void setbgcolor(uint8_t r, uint8_t g, uint8_t b);
     void setfgcolor(uint8_t r, uint8_t g, uint8_t b);
     void settext(const char *str);
@@ -22,8 +44,8 @@ private:
     SDL_Color backcolor;
     SDL_Color forecolor;
     SDL_Surface *text;
-    TTF_Font *font;
 
+    void setfontsize(int size);
     TText(const TText &);
     TText &operator=(const TText &);
 };
